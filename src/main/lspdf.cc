ecf5ab46e1243dc05eda925800de84f149980d29
takenori-y <takenori.yoshimura24@gmail.com>
2017-09-19 22:41:41 +0900

    Add lspdf command

---
+++ src/main/lspdf.cc
@@ -0,0 +1,264 @@
+// ----------------------------------------------------------------- //
+//             The Speech Signal Processing Toolkit (SPTK)           //
+//             developed by SPTK Working Group                       //
+//             http://sp-tk.sourceforge.net/                         //
+// ----------------------------------------------------------------- //
+//                                                                   //
+//  Copyright (c) 1984-2007  Tokyo Institute of Technology           //
+//                           Interdisciplinary Graduate School of    //
+//                           Science and Engineering                 //
+//                                                                   //
+//                1996-2017  Nagoya Institute of Technology          //
+//                           Department of Computer Science          //
+//                                                                   //
+// All rights reserved.                                              //
+//                                                                   //
+// Redistribution and use in source and binary forms, with or        //
+// without modification, are permitted provided that the following   //
+// conditions are met:                                               //
+//                                                                   //
+// - Redistributions of source code must retain the above copyright  //
+//   notice, this list of conditions and the following disclaimer.   //
+// - Redistributions in binary form must reproduce the above         //
+//   copyright notice, this list of conditions and the following     //
+//   disclaimer in the documentation and/or other materials provided //
+//   with the distribution.                                          //
+// - Neither the name of the SPTK working group nor the names of its //
+//   contributors may be used to endorse or promote products derived //
+//   from this software without specific prior written permission.   //
+//                                                                   //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND            //
+// CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,       //
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF          //
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE          //
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS //
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,          //
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED   //
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,     //
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON //
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,   //
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY    //
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           //
+// POSSIBILITY OF SUCH DAMAGE.                                       //
+// ----------------------------------------------------------------- //
+
+#include <getopt.h>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "SPTK/filter/line_spectral_pairs_digital_filter.h"
+#include "SPTK/input/input_source_from_stream.h"
+#include "SPTK/input/input_source_interpolation.h"
+#include "SPTK/input/input_source_preprocessing_for_filter_gain.h"
+#include "SPTK/utils/sptk_utils.h"
+
+namespace {
+
+const int kDefaultNumFilterOrder(25);
+const int kDefaultFramePeriod(100);
+const int kDefaultInterpolationPeriod(1);
+const sptk::InputSourcePreprocessingForFilterGain::FilterGainType
+    kDefaultGainType(
+        sptk::InputSourcePreprocessingForFilterGain::FilterGainType::kLinear);
+
+void PrintUsage(std::ostream* stream) {
+  // clang-format off
+  *stream << std::endl;
+  *stream << " lspdf - line spectral pairs digital filter for speech synthesis" << std::endl;  // NOLINT
+  *stream << std::endl;
+  *stream << "  usage:" << std::endl;
+  *stream << "       lspdf [ options ] lspfile [ infile ] > stdout" << std::endl;  // NOLINT
+  *stream << "  options:" << std::endl;
+  *stream << "       -m m  : order of filter coefficients (   int)[" << std::setw(5) << std::right << kDefaultNumFilterOrder      << "][ 0 <= m <=     ]" << std::endl;  // NOLINT
+  *stream << "       -p p  : frame period                 (   int)[" << std::setw(5) << std::right << kDefaultFramePeriod         << "][ 0 <  p <=     ]" << std::endl;  // NOLINT
+  *stream << "       -i i  : interpolation period         (   int)[" << std::setw(5) << std::right << kDefaultInterpolationPeriod << "][ 0 <= i <= p/2 ]" << std::endl;  // NOLINT
+  *stream << "       -k k  : gain type                    (   int)[" << std::setw(5) << std::right << kDefaultGainType            << "][ 0 <= k <= 2   ]" << std::endl;  // NOLINT
+  *stream << "                 0 (linear gain)" << std::endl;
+  *stream << "                 1 (log gain)" << std::endl;
+  *stream << "                 2 (unity gain)" << std::endl;
+  *stream << "       -h    : print this message" << std::endl;
+  *stream << "  infile:" << std::endl;
+  *stream << "       filter input                         (double)[stdin]" << std::endl;  // NOLINT
+  *stream << "  stdout:" << std::endl;
+  *stream << "       filter output                        (double)" << std::endl;  // NOLINT
+  *stream << "  lspfile:" << std::endl;
+  *stream << "       line spectral paris coefficients     (double)" << std::endl;  // NOLINT
+  *stream << "  notice:" << std::endl;
+  *stream << "       if i = 0, don't interpolate filter coefficients" << std::endl;  // NOLINT
+  *stream << std::endl;
+  *stream << " SPTK: version " << sptk::kVersion << std::endl;
+  *stream << std::endl;
+  // clang-format on
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  int num_filter_order(kDefaultNumFilterOrder);
+  int frame_period(kDefaultFramePeriod);
+  int interpolation_period(kDefaultInterpolationPeriod);
+  sptk::InputSourcePreprocessingForFilterGain::FilterGainType gain_type(
+      kDefaultGainType);
+
+  for (;;) {
+    const int option_char(getopt_long(argc, argv, "m:p:i:k:h", NULL, NULL));
+    if (-1 == option_char) break;
+
+    switch (option_char) {
+      case 'm': {
+        if (!sptk::ConvertStringToInteger(optarg, &num_filter_order) ||
+            num_filter_order < 0) {
+          std::ostringstream error_message;
+          error_message << "The argument for the -m option must be a "
+                        << "non-negative integer";
+          sptk::PrintErrorMessage("lspdf", error_message);
+          return 1;
+        }
+        break;
+      }
+      case 'p': {
+        if (!sptk::ConvertStringToInteger(optarg, &frame_period) ||
+            frame_period <= 0) {
+          std::ostringstream error_message;
+          error_message
+              << "The argument for the -p option must be a positive integer";
+          sptk::PrintErrorMessage("lspdf", error_message);
+          return 1;
+        }
+        break;
+      }
+      case 'i': {
+        if (!sptk::ConvertStringToInteger(optarg, &interpolation_period) ||
+            interpolation_period < 0) {
+          std::ostringstream error_message;
+          error_message << "The argument for the -i option must be a "
+                        << "non-negative integer";
+          sptk::PrintErrorMessage("lspdf", error_message);
+          return 1;
+        }
+        break;
+      }
+      case 'k': {
+        const int min(0);
+        const int max(static_cast<int>(
+            sptk::InputSourcePreprocessingForFilterGain::kUnity));
+        int tmp;
+        if (!sptk::ConvertStringToInteger(optarg, &tmp) ||
+            !sptk::IsInRange(tmp, min, max)) {
+          std::ostringstream error_message;
+          error_message << "The argument for the -k option must be an integer "
+                        << "in the range of " << min << " to " << max;
+          sptk::PrintErrorMessage("lspdf", error_message);
+          return 1;
+        }
+        gain_type = static_cast<
+            sptk::InputSourcePreprocessingForFilterGain::FilterGainType>(tmp);
+        break;
+      }
+      case 'h': {
+        PrintUsage(&std::cout);
+        return 0;
+      }
+      default: {
+        PrintUsage(&std::cerr);
+        return 1;
+      }
+    }
+  }
+
+  if (frame_period / 2 < interpolation_period) {
+    std::ostringstream error_message;
+    error_message << "Interpolation period must be equal to or less than half "
+                  << "frame period";
+    sptk::PrintErrorMessage("lspdf", error_message);
+    return 1;
+  }
+
+  // Get input file names.
+  const char* filter_coefficients_file;
+  const char* filter_input_file;
+  const int num_rest_args(argc - optind);
+  if (2 == num_rest_args) {
+    filter_coefficients_file = argv[argc - 2];
+    filter_input_file = argv[argc - 1];
+  } else if (1 == num_rest_args) {
+    filter_coefficients_file = argv[argc - 1];
+    filter_input_file = NULL;
+  } else {
+    std::ostringstream error_message;
+    error_message << "Just two input files, lspfile and infile, are required";
+    sptk::PrintErrorMessage("lspdf", error_message);
+    return 1;
+  }
+
+  // Open stream for reading filter coeffcients.
+  std::ifstream ifs1;
+  ifs1.open(filter_coefficients_file, std::ios::in | std::ios::binary);
+  if (ifs1.fail()) {
+    std::ostringstream error_message;
+    error_message << "Cannot open file " << filter_coefficients_file;
+    sptk::PrintErrorMessage("lspdf", error_message);
+    return 1;
+  }
+  std::istream& stream_for_filter_coefficients(ifs1);
+
+  // Open stream for reading input signals.
+  std::ifstream ifs2;
+  ifs2.open(filter_input_file, std::ios::in | std::ios::binary);
+  if (ifs2.fail() && NULL != filter_input_file) {
+    std::ostringstream error_message;
+    error_message << "Cannot open file " << filter_input_file;
+    sptk::PrintErrorMessage("lspdf", error_message);
+    return 1;
+  }
+  std::istream& stream_for_filter_input(ifs2.fail() ? std::cin : ifs2);
+
+  // Prepare variables for filtering.
+  const int filter_length(num_filter_order + 1);
+  std::vector<double> filter_coefficients(filter_length);
+  sptk::InputSourceFromStream input_source(false, filter_length,
+                                           &stream_for_filter_coefficients);
+  sptk::InputSourcePreprocessingForFilterGain preprocessing(gain_type,
+                                                            &input_source);
+  sptk::InputSourceInterpolation interpolation(
+      frame_period, interpolation_period, true, &preprocessing);
+  double filter_input, filter_output;
+  sptk::LineSpectralPairsDigitalFilter filter(num_filter_order);
+  sptk::LineSpectralPairsDigitalFilter::StoredSignals stored_signals;
+
+  if (!interpolation.IsValid() || !filter.IsValid()) {
+    std::ostringstream error_message;
+    error_message << "Failed to set condition for filtering";
+    sptk::PrintErrorMessage("lspdf", error_message);
+    return 1;
+  }
+
+  while (sptk::ReadStream(&filter_input, &stream_for_filter_input)) {
+    if (!interpolation.Get(&filter_coefficients)) {
+      std::ostringstream error_message;
+      error_message << "Cannot get filter coefficients";
+      sptk::PrintErrorMessage("lspdf", error_message);
+      return 1;
+    }
+
+    if (!filter.Run(filter_coefficients, filter_input, &filter_output,
+                    &stored_signals)) {
+      std::ostringstream error_message;
+      error_message << "Failed to apply line spectral pairs digital filter";
+      sptk::PrintErrorMessage("lspdf", error_message);
+      return 1;
+    }
+
+    if (!sptk::WriteStream(filter_output, &std::cout)) {
+      std::ostringstream error_message;
+      error_message << "Failed to write a filter output";
+      sptk::PrintErrorMessage("lspdf", error_message);
+      return 1;
+    }
+  }
+
+  return 0;
+}
