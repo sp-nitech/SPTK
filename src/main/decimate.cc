426a4a5378360373016b5bdbe9ecd2a6a7e44732
Natsumi Koike <koike.n@sp.nitech.ac.jp>
2017-10-30 16:35:59 +0900

    argument check

--- src/main/decimate.cc
+++ src/main/decimate.cc
@@ -136,7 +136,14 @@ int main(int argc, char* argv[]) {
   }
 
   // get input file
-  const char* input_file((optind < argc) ? argv[argc - 1] : NULL);
+  const int num_rest_args(argc - optind);
+  if (1 < num_rest_args) {
+    std::ostringstream error_message;
+    error_message << "Too many input files";
+    sptk::PrintErrorMessage("decimate", error_message);
+    return 1;
+  }
+  const char* input_file(0 == num_rest_args ? NULL : argv[optind]);
 
   // open stream
   std::ifstream ifs;
