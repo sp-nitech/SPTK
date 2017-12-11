8ef0719b4533918b3db6382cc72914bd61770e35
takenori-y <takenori.yoshimura24@gmail.com>
2017-12-11 18:30:59 +0900

    Add sin command

---
+++ src/main/sin.cc
@@ -0,0 +1,171 @@
+// ----------------------------------------------------------------- //
+//             The Speech Signal Processing Toolkit (SPTK)           //
+//             developed by SPTK Working Group                       //
+//             http://sp-tk.sourceforge.net/                         //
+// ----------------------------------------------------------------- //
+//                                                                   //
+//  Copyright (c) 1984-2007  Tokyo Institute of Technology           //
+//                           Interdisciplinary Graduate School of    //
+//                           Science and Engineering                 //
+//                                                                   //
+//                1996-2017  Nagoya Institute of Technology          //
+//                           Department of Computer Science          //
+//                                                                   //
+// All rights reserved.                                              //
+//                                                                   //
+// Redistribution and use in source and binary forms, with or        //
+// without modification, are permitted provided that the following   //
+// conditions are met:                                               //
+//                                                                   //
+// - Redistributions of source code must retain the above copyright  //
+//   notice, this list of conditions and the following disclaimer.   //
+// - Redistributions in binary form must reproduce the above         //
+//   copyright notice, this list of conditions and the following     //
+//   disclaimer in the documentation and/or other materials provided //
+//   with the distribution.                                          //
+// - Neither the name of the SPTK working group nor the names of its //
+//   contributors may be used to endorse or promote products derived //
+//   from this software without specific prior written permission.   //
+//                                                                   //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND            //
+// CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,       //
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF          //
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE          //
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS //
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,          //
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED   //
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,     //
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON //
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,   //
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY    //
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           //
+// POSSIBILITY OF SUCH DAMAGE.                                       //
+// ----------------------------------------------------------------- //
+
+#include <getopt.h>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+#include "SPTK/utils/sptk_utils.h"
+
+namespace {
+
+const int kDefaultOutputLength(256);
+const double kDefaultPeriod(10.0);
+const double kDefaultMagnitude(1.0);
+const bool kDefaultCosineWaveFlag(false);
+
+void PrintUsage(std::ostream* stream) {
+  // clang-format off
+  *stream << std::endl;
+  *stream << " sin - generate sinusoidal sequence" << std::endl;
+  *stream << std::endl;
+  *stream << "  usage:" << std::endl;
+  *stream << "       sin [ options ] > stdout" << std::endl;
+  *stream << "  options:" << std::endl;
+  *stream << "       -l l  : output length      (   int)[" << std::setw(5) << std::right << kDefaultOutputLength << "][     <= l <=   ]" << std::endl;  // NOLINT
+  *stream << "       -n n  : output order       (   int)[" << std::setw(5) << std::right << "l-1"                << "][     <= n <=   ]" << std::endl;  // NOLINT
+  *stream << "       -p p  : period             (double)[" << std::setw(5) << std::right << kDefaultPeriod       << "][ 0.0 <  p <=   ]" << std::endl;  // NOLINT
+  *stream << "       -m m  : magnitude          (double)[" << std::setw(5) << std::right << kDefaultMagnitude    << "][     <= m <=   ]" << std::endl;  // NOLINT
+  *stream << "       -C    : cosine wave        (  bool)[" << std::setw(5) << std::right << sptk::ConvertBooleanToString(kDefaultCosineWaveFlag) << "]" << std::endl;  // NOLINT
+  *stream << "       -h    : print this message" << std::endl;
+  *stream << "  stdout:" << std::endl;
+  *stream << "       sinusoidal sequence        (double)" << std::endl;
+  *stream << "  notice:" << std::endl;
+  *stream << "       if l <= 0 or n < 0, generate infinite sequence" << std::endl;  // NOLINT
+  *stream << std::endl;
+  *stream << " SPTK: version " << sptk::kVersion << std::endl;
+  *stream << std::endl;
+  // clang-format on
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  int output_length(kDefaultOutputLength);
+  double period(kDefaultPeriod);
+  double magnitude(kDefaultMagnitude);
+  bool cosine_wave(kDefaultCosineWaveFlag);
+
+  for (;;) {
+    const int option_char(getopt_long(argc, argv, "l:n:p:m:Ch", NULL, NULL));
+    if (-1 == option_char) break;
+
+    switch (option_char) {
+      case 'l': {
+        if (!sptk::ConvertStringToInteger(optarg, &output_length)) {
+          std::ostringstream error_message;
+          error_message << "The argument for the -l option must be integer";
+          sptk::PrintErrorMessage("sin", error_message);
+          return 1;
+        }
+        break;
+      }
+      case 'n': {
+        if (!sptk::ConvertStringToInteger(optarg, &output_length)) {
+          std::ostringstream error_message;
+          error_message << "The argument for the -n option must be integer";
+          sptk::PrintErrorMessage("sin", error_message);
+          return 1;
+        }
+        ++output_length;
+        break;
+      }
+      case 'p': {
+        if (!sptk::ConvertStringToDouble(optarg, &period) || period <= 0.0) {
+          std::ostringstream error_message;
+          error_message
+              << "The argument for the -p option must be a positive number";
+          sptk::PrintErrorMessage("sin", error_message);
+          return 1;
+        }
+        break;
+      }
+      case 'm': {
+        if (!sptk::ConvertStringToDouble(optarg, &magnitude)) {
+          std::ostringstream error_message;
+          error_message << "The argument for the -m option must be numeric";
+          sptk::PrintErrorMessage("sin", error_message);
+          return 1;
+        }
+        break;
+      }
+      case 'C': {
+        cosine_wave = true;
+        break;
+      }
+      case 'h': {
+        PrintUsage(&std::cout);
+        return 0;
+      }
+      default: {
+        PrintUsage(&std::cerr);
+        return 1;
+      }
+    }
+  }
+
+  if (0 != argc - optind) {
+    std::ostringstream error_message;
+    error_message << "Input file is not required";
+    sptk::PrintErrorMessage("sin", error_message);
+    return 1;
+  }
+
+  const double frequency(1.0 / period);
+  for (int i(0); output_length <= 0 || i < output_length; ++i) {
+    const double output(
+        cosine_wave ? magnitude * std::cos(sptk::kTwoPi * frequency * i)
+                    : magnitude * std::sin(sptk::kTwoPi * frequency * i));
+    if (!sptk::WriteStream(output, &std::cout)) {
+      std::ostringstream error_message;
+      error_message << "Failed to write sinusoidal sequence";
+      sptk::PrintErrorMessage("sin", error_message);
+      return 1;
+    }
+  }
+
+  return 0;
+}
