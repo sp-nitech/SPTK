f7b2fd06350a21f6f3850d73bbd1f85a4f73a505
takenori-y <takenori.yoshimura24@gmail.com>
2018-01-16 12:32:03 +0900

    Update copyright

--- src/main/par2lpc.cc
+++ src/main/par2lpc.cc
@@ -8,7 +8,7 @@
 //                           Interdisciplinary Graduate School of    //
 //                           Science and Engineering                 //
 //                                                                   //
-//                1996-2017  Nagoya Institute of Technology          //
+//                1996-2018  Nagoya Institute of Technology          //
 //                           Department of Computer Science          //
 //                                                                   //
 // All rights reserved.                                              //
