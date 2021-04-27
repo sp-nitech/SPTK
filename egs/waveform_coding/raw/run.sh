87c01cdb79ba6c118b82a1c687c3dc8d949ba20a
takenori-y <takenori.yoshimura24@gmail.com>
2021-04-27 17:53:21 +0900

    add new example [skip ci]

---
+++ egs/waveform_coding/raw/run.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# ----------------------------------------------------------------- #
+#             The Speech Signal Processing Toolkit (SPTK)           #
+#             developed by SPTK Working Group                       #
+#             http://sp-tk.sourceforge.net/                         #
+# ----------------------------------------------------------------- #
+#                                                                   #
+#  Copyright (c) 1984-2007  Tokyo Institute of Technology           #
+#                           Interdisciplinary Graduate School of    #
+#                           Science and Engineering                 #
+#                                                                   #
+#                1996-2020  Nagoya Institute of Technology          #
+#                           Department of Computer Science          #
+#                                                                   #
+# All rights reserved.                                              #
+#                                                                   #
+# Redistribution and use in source and binary forms, with or        #
+# without modification, are permitted provided that the following   #
+# conditions are met:                                               #
+#                                                                   #
+# - Redistributions of source code must retain the above copyright  #
+#   notice, this list of conditions and the following disclaimer.   #
+# - Redistributions in binary form must reproduce the above         #
+#   copyright notice, this list of conditions and the following     #
+#   disclaimer in the documentation and/or other materials provided #
+#   with the distribution.                                          #
+# - Neither the name of the SPTK working group nor the names of its #
+#   contributors may be used to endorse or promote products derived #
+#   from this software without specific prior written permission.   #
+#                                                                   #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND            #
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,       #
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF          #
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE          #
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS #
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,          #
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED   #
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,     #
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON #
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,   #
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY    #
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           #
+# POSSIBILITY OF SUCH DAMAGE.                                       #
+# ----------------------------------------------------------------- #
+
+set -euo pipefail
+
+sptk4=../../../bin
+data=../../../asset/data.short
+dump=dump
+
+nbit=8  # Number of bits
+
+mkdir -p $dump
+
+# Quantize.
+$sptk4/x2x +sd $data | \
+   $sptk4/ulaw | \
+   $sptk4/quantize -b $nbit > $dump/data.q
+
+# Generate codebook.
+b=$(echo 2 | $sptk4/x2x +ad | $sptk4/sopr -p $nbit | $sptk4/x2x +da)
+$sptk4/x2x +id $dump/data.q | \
+   $sptk4/histogram -b $b -l 0 -u $(($b-1)) | \
+   $sptk4/huffman > $dump/data.cb
+
+# Encode and transmit.
+$sptk4/huffman_encode $dump/data.cb $dump/data.q > $dump/data.enc
+
+# Recieve and decode.
+$sptk4/huffman_decode $dump/data.cb $dump/data.enc > $dump/data.dec
+
+# Dequantize.
+$sptk4/dequantize -b $nbit $dump/data.dec | \
+   $sptk4/iulaw | \
+   $sptk4/x2x +ds > $dump/data.raw
+
+# Check size.
+org_size=$(stat -c %s $data | $sptk4/x2x +ad | \
+                $sptk4/sopr -m 8 | $sptk4/x2x +da)
+ulaw_size=$(stat -c %s $dump/data.q | $sptk4/x2x +ad | \
+                 $sptk4/sopr -m 2 | $sptk4/x2x +da)
+huff_size=$(stat -c %s $dump/data.enc)
+
+echo "Original size: $org_size bits"
+echo "Quantized size: $ulaw_size bits"
+echo "Huffman size: $huff_size bits"
