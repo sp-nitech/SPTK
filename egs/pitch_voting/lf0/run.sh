e22f6b37c606e32d66420671a16df226fc182bed
takenori-y <takenori.yoshimura24@gmail.com>
2022-07-07 23:19:55 +0900

    make shellcheck happy

--- egs/pitch_voting/lf0/run.sh
+++ egs/pitch_voting/lf0/run.sh
@@ -29,7 +29,7 @@ mkdir -p $dump
 # Extract pitch.
 for a in $(seq 0 3); do
     $sptk4/x2x +sd $data |
-        $sptk4/pitch -s $sr -p $fp -o 2 -a $a > $dump/data.lf0.$a
+        $sptk4/pitch -s $sr -p $fp -o 2 -a "$a" > $dump/data.lf0."$a"
 done
 
 # Perform voting.
@@ -39,11 +39,12 @@ $sptk4/merge -l 1 -L 1 $dump/data.lf0.1 < $dump/data.lf0.0 |
     $sptk4/medfilt -l 4 -k 2 -magic -1e+10 -w 1 > $dump/data.lf0
 
 # Draw pitch contours.
+# shellcheck disable=SC1091
 . ../../../tools/venv/bin/activate
 n=$($sptk4/x2x +da $dump/data.lf0 | wc -l)
 cat $dump/data.lf0.? $dump/data.lf0 |
     $sptk4/sopr -magic -1e+10 -EXP -MAGIC 0 |
-    $sptk4/fdrw -n $n -g $dump/contour.png \
+    $sptk4/fdrw -n "$n" -g $dump/contour.png \
                 -xname "Time [frame]" \
                 -yname "Fundamental frequency [Hz]" \
                 -names "RAPT,SWIPE,REAPER,WORLD,Voting"
